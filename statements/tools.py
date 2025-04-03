1d949198ef113848d6f69bcc8aadcb35d6f75e4a
przemyslawmazurkiewicz <przemyslaw.mazurkiewicz@eclipsegroup.co.uk>
2025-04-03 15:32:26 +0200

    Add cache for already added accounts and statements

--- statements/tools.py
+++ statements/tools.py
@@ -7,18 +7,31 @@ def statement_import(file_handler):
     idx = 0
     # TODO: TASK → in case of errors database must not change
     # TODO: TASK → optimize database queries during import
+    #I would use here Redis, below dicts are only for simplicity purposes. Main goal is that we have to reduce db queries.
+    #Simple logic for caching, if account is not in cache, we create it and add to cache, same with statements
+    accounts_cache = {}
+    statements_cache = {}
     for row in csv.DictReader(file_handler):
-        account = Account.objects.get_or_create(
-            name=row['account'],
-            defaults={'currency': row['currency']}
-        )[0]
-        if account.currency != row['currency']:
-            raise ValidationError('Invalid currency currency ')
-
-        statement = Statement.objects.get_or_create(
-            account=account,
-            date=row['date']
-        )[0]
+        account_key = (row['account'], row['currency'])
+        if account_key not in accounts_cache:
+            account = Account.objects.get_or_create(
+                name=row['account'],
+                defaults={'currency': row['currency']}
+            )[0]
+            if account.currency != row['currency']:
+                raise ValidationError('Invalid currency currency ')
+            accounts_cache[account_key] = account
+        else:
+            account = accounts_cache[account_key]
+        statement_key = (account.id, row['date'])
+        if statement_key not in statements_cache:
+            statement = Statement.objects.get_or_create(
+                account=account,
+                date=row['date']
+            )[0]
+            statements_cache[statement_key] = statement
+        else:
+            statement = statements_cache[statement_key]
         StatementItem.objects.create(
             statement=statement,
             amount=row['amount'],
